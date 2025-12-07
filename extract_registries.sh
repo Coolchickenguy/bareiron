eecc581f32ae0d95260ff7c6e7921ab5e1597527
Coolchickenguy <coolchickenguy@protonmail.com>
2025-12-06 17:38:49 -0700

    Auto-download notchian

--- extract_registries.sh
+++ extract_registries.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 REQUIRED_MAJOR=21
 SERVER_JAR="${SERVER_JAR:-server.jar}"
 NOTCHIAN_DIR="notchian"
+NOTCHIAN_VERSION="${NOTCHIAN_VERSION:-1.21.8}"
 JS_RUNTIME=""
 
 get_java_version() {
@@ -25,6 +26,20 @@ check_java() {
   fi
 }
 
+check_wget() {
+  if ! command -v wget>/dev/null 2>&1; then
+    echo "Wget not found in path"
+    exit 1
+  fi
+}
+
+check_jq() {
+  if ! command -v wget>/dev/null 2>&1; then
+    echo "Jq not found in path"
+    exit 1
+  fi
+}
+
 prepare_notchian_dir() {
   if [[ ! -d "$NOTCHIAN_DIR" ]]; then
     echo "Creating $NOTCHIAN_DIR directory..."
@@ -36,9 +51,13 @@ prepare_notchian_dir() {
 dump_registries() {
   if [[ ! -f "$SERVER_JAR" ]]; then
     echo "No server.jar found (looked for $SERVER_JAR)."
-	echo "Please download the 1.21.8 server.jar (e.g. from https://mcversions.net/download/1.21.8)"
-	echo "and place it in the \"notchian\" directory."
-    exit 1
+	  echo "Downloading version $NOTCHIAN_VERSION (change by setting NOTCHIAN_VERSION)"
+    check_wget
+    check_jq
+
+    SERVER_MANIFEST_URL="$(wget -qO- "https://piston-meta.mojang.com/mc/game/version_manifest.json" | jq -r ".versions[] | select(.id == \"${NOTCHIAN_VERSION}\") | .url")"
+    SERVER_JAR_URL="$(wget -qO- "$SERVER_MANIFEST_URL" | jq -r ".downloads.server.url")"
+    wget -O server.jar "$SERVER_JAR_URL"
   fi
 
   java -DbundlerMainClass="net.minecraft.data.Main" -jar "$SERVER_JAR" --all
